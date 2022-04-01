04a2d53d968652ab097192df9ffc80dc1bf3c4c3
Regtables <reghardt7@gmail.com>
2022-04-01 17:33:14 +0200

    layout complete, primed elements for parallax effect

--- src/containers/heroDemo/HeroDemo.js
+++ src/containers/heroDemo/HeroDemo.js
@@ -2,12 +2,22 @@ import React from 'react'
 
 import './heroDemo.scss'
 import logo from '../../assets/EnjoyHeroLogoGrey.svg'
+import video from '../../assets/showreel.mp4'
 
 const HeroDemo = () => {
+
+  // window.onscroll = function () { window.scrollTo(0, 0); };
+
   return (
     <div className = 'heroDemo-container'>
+        <video src = {video} autoPlay muted loop>
+
+        </video>
         <div className = 'logo-container'>
             <img src = {logo}></img>
+            <div className = 'logo-scroll-origin'>
+
+            </div>
         </div>
         <div className = 'scroll-container'>
             <div className = 'scroll'>
