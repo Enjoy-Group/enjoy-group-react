a205d71ac99c658336f34a33323cc6e7e95fd4e0
Regtables <reghardt7@gmail.com>
2022-04-04 10:32:48 +0200

    hero demo

---
+++ src/containers/hero/Hero.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import './hero.scss'
+import logo from '../../assets/EnjoyHeroLogoGrey.svg'
+import video from '../../assets/showreel.mp4'
+import animatedLogo from '../../assets/enjoyAnimated.mov'
+
+const HeroDemo = () => {
+
+  // window.onscroll = function () { window.scrollTo(0, 0); };
+
+  return (
+    <div className = 'hero-container'>
+        {/* <video src = {video} autoPlay muted loop>
+
+        </video> */}
+        <div className = 'logo-container'>
+            <img src = {logo}></img>
+            {/* <div className = 'logo-scroll-origin'>
+
+            </div> */}
+        </div>
+        <div className = 'scroll-container'>
+            <div className = 'scroll'>
+                <p>scroll</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroDemo
