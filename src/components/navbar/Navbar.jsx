8373279709695a1dcfa44c6036a5cbec2bda3b2b
Regtables <reghardt7@gmail.com>
2022-04-04 11:30:22 +0200

    added fixed navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { FiMenu } from 'react-icons/fi'
+import { CgMenu } from 'react-icons/cg'
+import { IoMenuOutline } from 'react-icons/io5'
+
+import './navbar.scss'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className = 'enjoy__navbar'>
+        <div className = 'enjoy__navbar-container'>
+            <CgMenu color = '#39FF14' size = {64} fontWeight = {100}/>
+        </div>
+    </nav>
   )
 }
 
