0d10ee2584d5cfd935deaec370c568a41f9965ea
Regtables <reghardt7@gmail.com>
2022-04-07 12:18:42 +0200

    hover effects on buttons

--- src/components/buttonMain/ButtonMain.jsx
+++ src/components/buttonMain/ButtonMain.jsx
@@ -1,38 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import { motion } from 'framer-motion'
 
 import './buttonMain.scss'
 
 const ButtonMain = ({text, marginRight, marginLeft }) => {
-  function renderButton(){
-    if(marginRight){
-      console.log('right')
-      return (
-        <div className = 'enjoy__buttonMain' role = 'button' style = {{marginRight: 'auto'}}>
-            <p>{text}</p>
-        </div>
-      )
-    } 
-    if(marginLeft){
-      console.log('left')
-      return (
-        <div className = 'enjoy__buttonMain' role = 'button' style = {{marginleft: 'auto'}}>
-            <p>{text}</p>
-        </div>
-      )
-    } 
-    if(!marginRight && !marginLeft){
-      return (
-        <div className = 'enjoy__buttonMain' role = 'button'>
-            <p>{text}</p>
-        </div>
-      )
-    }
+  const [animateDiv, setAnimateDiv] = useState({})
+  const [animateText, setAnimateText] = useState({})
+
+  function onHoverStartAnimate(){
+    setAnimateText({marginLeft: 0, boxShadow: 'rgb(24 24 24) 0px 0px 0px 0px', scale: 0.9, width: 'auto' });
+    setAnimateDiv({scale: 1.2})
   }
+
+  function onHoverExitAnimate(){
+    setAnimateText({marginLeft: '50px', boxShadow: 'rgb(24 24 24) 0px 0px 0px 10px', scale: 1, width: '100px' })
+    setAnimateDiv({scale: 1, justifyContent: 'unset'})
+  }
+
   return (
-    <>
-    {renderButton()}
-    </>
+    <motion.div
+      className = 'enjoy__buttonMain' role = 'button'
+      onHoverStart = {onHoverStartAnimate}
+      onHoverEnd = {onHoverExitAnimate}
+      animate = {animateDiv}
+      transition = {{duration: 0.3, ease: 'easeInOut'}}
+      id = 'button'
+      >
+        <motion.p
+          animate = {animateText}
+          transition = {{duration: 0.3, ease: 'easeInOut'}}
+          id = 'button-text'
+        >
+          {text}</motion.p>
+    </motion.div>
   )
 }
 
